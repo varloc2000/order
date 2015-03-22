440d64736bc03c7a7fbecd0b1654fa0ed777e5d6
Timofey Cherniavsky <varloc2000@gmail.com>
2015-03-22 23:11:33 +0300

    Delete useless file. Add deploy config.

---
+++ app/config/deploy.rb
@@ -0,0 +1,10 @@
+#APP
+set :application, "Orders"
+set :app_path, "."
+set :scm, :none
+
+#FTP
+set :login, "varloc2000"
+set :password, "Varloc80177413"
+set :ftp_host, "93.85.85.197"
+set :deploy_to, "/www/order.rats-records.by"
